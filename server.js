3337179393bdc493bf7a3d624137a16f286492c4
Frail Bongat <frail.bongat@gmail.com>
2023-07-24 07:47:18 +0800

    build: initialize npm

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  // lodash
+  const num = _.random(0, 20);
+  console.log(num);
+
+  const greet = _.once(() => {
+    console.log('hello');
+  });
+
+  greet();
+  greet();
 
   // set header content type
   res.setHeader('Content-Type', 'text/html');
@@ -11,12 +21,20 @@ const server = http.createServer((req, res) => {
   switch (req.url) {
     case '/':
       path += '/index.html';
+      res.statusCode = 200;
       break;
     case '/about':
       path += '/about.html';
+      res.statusCode = 200;
+      break;
+    case '/about-me':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
       break;
     default:
       path += '/404.html';
+      res.statusCode = 404;
   }
 
   // res.write('<head><link rel="stylesheet" href="#"></head>');
